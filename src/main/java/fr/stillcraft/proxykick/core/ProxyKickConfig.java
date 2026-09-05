090a079123162c430880e128df2a959d3c6ac898
Augustin Blanchet <32638090+augstb@users.noreply.github.com>
2026-09-05 23:32:57 +0200

    Bump version to 1.3.1

--- src/main/java/fr/stillcraft/proxykick/core/ProxyKickConfig.java
+++ src/main/java/fr/stillcraft/proxykick/core/ProxyKickConfig.java
@@ -11,7 +11,7 @@ import java.util.function.Consumer;
  */
 public final class ProxyKickConfig {
     // Version (don't forget to increment)
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
 
     // Used config files keys
     private static final String[] locale_keys = {
